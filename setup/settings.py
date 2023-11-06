1811fd80d04e135cee3b1c96f94dcf118357ed75
Henrique <henriqueribeiro88bb@gmail.com>
2023-11-06 14:52:33 -0300

    projeto-alura-space

--- setup/settings.py
+++ setup/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,7 +127,23 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
+
+#MEDIA
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Messages
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
